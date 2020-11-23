6a85fb07ee65868c81a3a9921a5e581557fc7834
Konata09 <lz9709@gmail.com>
2020-11-23 17:43:25 +0800

    change default sample to paimon

--- waifu-tips.js
+++ waifu-tips.js
@@ -22,7 +22,7 @@ const live2d_settings = {
     'modelUrl': 'model',                        // 存放模型的文件夹路径，末尾不需要斜杠
     'tipsMessage': 'waifu-tips.json',           // 看板娘提示消息文件的路径，可以留空不加载
     // 模型设置
-    'modelName': 'miku',                        // 默认加载的模型名称，仅在无本地记录的情况下有效
+    'modelName': 'paimeng',                     // 默认加载的模型名称，仅在无本地记录的情况下有效
     'modelStorage': true,                       // 记忆模型，下次打开页面会加载上次选择的模型
     'modelRandMode': false,                     // 随机切换模型
     'preLoadMotion': true,                      // 是否预载动作数据，只对 model3 模型有效，不预载可以提高 model3 模型的加载速度，但可能导致首次触发动作时卡顿
